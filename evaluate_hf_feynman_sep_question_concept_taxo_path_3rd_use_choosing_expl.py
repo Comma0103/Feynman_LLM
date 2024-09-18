4b658663c1d1feab49a3b2bb961abff143e536f6
qilongma <jwl42mql@163.com>
2024-09-18 01:36:54 -0700

    wo_choosing_3rd_use_choosing_expl

--- evaluate_hf_feynman_sep_question_concept_taxo_path_3rd_use_choosing_expl.py
+++ evaluate_hf_feynman_sep_question_concept_taxo_path_3rd_use_choosing_expl.py
@@ -45,29 +45,30 @@ def gen_prompt(train_df, subject, k=-1):
         prompt += format_example(train_df, i)
     return prompt
 
-def gen_2nd_expl_request(concept_with_taxo_path, old_concept_expl):
-    request = (
-        "Assume you are a teacher or an LLM trainer, tasked with teaching concepts of various disciplines.\n\n" + \
-        "There is a concept(left part of ':') with its path in the taxonomy system(right part of ':'), related to some question:\n" + \
-        "{0}\n" + \
-        "where the 4 levels in the taxonomy path are 'discipline -> subject -> class session -> knowledge point' if the path is found, otherwise, understand it according to your knowledge.\n\n" + \
-        "You used to give the explanation of this concept as follow:\n" + \
-        "{1}\n\n" + \
-        "However, a student LM got a wrong answer after adding your explanation into its prompt when answering the question.\n\n" + \
-        "Based on your understanding of this concept, think about the reason why the student got the wrong answer, " + \
-        "could you modify the explanation of this concept to teach someone who is not familiar with it, " + \
-        "or a smaller language model, so that they can understand it easily then answer the question right?\n\n" + \
-        "Just simply explain the concept itself (NOT knowledge point) using the format 'concept: explanation' within 32 tokens, DO NOT use any font format or extra words:"
-    ).format(concept_with_taxo_path, old_concept_expl)
-    return request
+# def gen_3nd_expl_request(concept_with_taxo_path, second_concept_expl):
+#     request = (
+#         "Assume you are a teacher or an LLM trainer, tasked with teaching concepts of various disciplines.\n\n" + \
+#         "There is a concept(left part of ':') with its path in the taxonomy system(right part of ':'), related to some question:\n" + \
+#         "{0}\n" + \
+#         "where the 4 levels in the taxonomy path are 'discipline -> subject -> class session -> knowledge point' if the path is found, otherwise, understand it according to your knowledge.\n\n" + \
+#         "You used to give the explanation of this concept as follow:\n" + \
+#         "{1}\n\n" + \
+#         "However, a student LM got a wrong answer after adding your explanation into its prompt when answering the question.\n\n" + \
+#         "Based on your understanding of this concept, think about the reason why the student got the wrong answer, " + \
+#         "could you modify the explanation of this concept to teach someone who is not familiar with it, " + \
+#         "or a smaller language model, so that they can understand it easily then answer the question right?\n\n" + \
+#         "Just simply explain the concept itself (NOT knowledge point) using the format 'concept: explanation' within 32 tokens, DO NOT use any font format or extra words:"
+#     ).format(concept_with_taxo_path, second_concept_expl)
+#     return request
 
-def gen_expl_prompt(concepts=None, taxonomy_paths=None, explanations=None):
-    if not pd.isna(concepts) and not pd.isna(taxonomy_paths) and not pd.isna(explanations):
+def gen_expl_prompt(concepts=None, taxonomy_paths=None, third_explanations=None, new_explanations=None, explanations=None):
+    if not pd.isna(concepts) and not pd.isna(taxonomy_paths) and (not pd.isna(third_explanations) or not pd.isna(new_explanations) or not pd.isna(explanations)):
         expl_prompt = "Here are some concepts with their path in the taxonomy system (if not found, understand according to your knowledge), " + \
                 "and explanations of the concepts (if not available, understand according to your knowledge), which may be useful:\n"
         concepts_list = concepts.split(", ")
         taxonomy_paths_list = [e.split(": ")[1] if ": " in e else e for e in taxonomy_paths.split(";|; ")]
-        explanations_list = [e.split(": ")[1] if ": " in e else e for e in explanations.split(";|; ")]
+        explanations_used = third_explanations if not pd.isna(third_explanations) else new_explanations if not pd.isna(new_explanations) else explanations
+        explanations_list = [e.split(": ")[1] if ": " in e else e for e in explanations_used.split(";|; ")]
         for concept, taxo_path, expl in zip(concepts_list, taxonomy_paths_list, explanations_list):
             expl_prompt += "Concept: {}, Taxonomy Path: {}, Explanation: {}\n".format(concept, taxo_path, expl)
         expl_prompt += "\n"
@@ -110,11 +111,11 @@ def forward(args, model, tokenizer, input_ids):
     pred = {0: "A", 1: "B", 2: "C", 3: "D"}[np.argmax(probs)]
     return probs, pred
 
-def eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_expls_df, teacher_client):
+def eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_expls_df):
     cors = []
     all_probs = []
     answers = choices[:test_df.shape[1]-2]
-    all_new_expls = []
+    # all_third_expls = []
 
     for i in tqdm(range(test_df.shape[0]), ncols=75):
         # get prompt and make sure it fits
@@ -125,6 +126,8 @@ def eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_ex
         expl_prompt = gen_expl_prompt(
             test_expls_df.loc[i, "concepts"],
             test_expls_df.loc[i, "taxonomy_path"],
+            test_expls_df.loc[i, "third_explanations"],
+            test_expls_df.loc[i, "new_explanations"],
             test_expls_df.loc[i, "explanations"]
         )
         prompt = expl_prompt + train_prompt + prompt_end
@@ -141,37 +144,37 @@ def eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_ex
         probs, pred = forward(args, model, tokenizer, input_ids)
         cor = pred == label
         
-        if not cor:
-            if pd.isna(test_expls_df.loc[i, "taxonomy_path"]):
-                new_expls = None
-                all_new_expls.append(new_expls)
-            else:
-                concepts = test_expls_df.loc[i, "concepts"].split(", ")
-                concepts_with_taxo_path = test_expls_df.loc[i, "taxonomy_path"].split(";|; ")
-                if pd.isna(test_expls_df.loc[i, "explanations"]):
-                    old_expls = [concept + ": Not available" for concept in concepts]
-                else:
-                    old_expls = test_expls_df.loc[i, "explanations"].split(";|; ")
-                new_expls = []
-                for concept, concept_with_taxo_path, old_expl in zip(concepts, concepts_with_taxo_path, old_expls):
-                    if "Not found" in concept_with_taxo_path:
-                        concept_with_taxo_path = concept + ": Not found"
-                    # get prompt and make sure it fits
-                    new_expl = teacher_client.call(gen_2nd_expl_request(concept_with_taxo_path, old_expl)) 
-                    new_expls.append(new_expl if new_expl is not None else concept + ": Not available")
-                all_new_expls.append(";|; ".join(new_expls))
+        # if not cor:
+        #     if pd.isna(test_expls_df.loc[i, "taxonomy_path"]):
+        #         third_expls = None
+        #         all_third_expls.append(third_expls)
+        #     else:
+        #         concepts = test_expls_df.loc[i, "concepts"].split(", ")
+        #         concepts_with_taxo_path = test_expls_df.loc[i, "taxonomy_path"].split(";|; ")
+        #         if pd.isna(test_expls_df.loc[i, "new_explanations"]):
+        #             second_expls = [concept + ": Not available" for concept in concepts]
+        #         else:
+        #             second_expls = test_expls_df.loc[i, "new_explanations"].split(";|; ")
+        #         third_expls = []
+        #         for concept, concept_with_taxo_path, second_expl in zip(concepts, concepts_with_taxo_path, second_expls):
+        #             if "Not found" in concept_with_taxo_path:
+        #                 concept_with_taxo_path = concept + ": Not found"
+        #             # get prompt and make sure it fits
+        #             third_expl = teacher_client.call(gen_3rd_expl_request(concept_with_taxo_path, second_expl)) 
+        #             third_expls.append(third_expl if third_expl is not None else concept + ": Not available")
+        #         all_third_expls.append(";|; ".join(third_expls))
             
-            expl_prompt = gen_expl_prompt(
-                test_expls_df.loc[i, "concepts"],
-                test_expls_df.loc[i, "taxonomy_path"],
-                ";|; ".join(new_expls) if new_expls is not None else None
-            )
-            prompt = expl_prompt + train_prompt + prompt_end
-            input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
-            probs, pred = forward(args, model, tokenizer, input_ids)
-            cor = pred == label
-        else:
-            all_new_expls.append(None)
+        #     expl_prompt = gen_expl_prompt(
+        #         test_expls_df.loc[i, "concepts"],
+        #         test_expls_df.loc[i, "taxonomy_path"],
+        #         ";|; ".join(third_expls) if third_expls is not None else None
+        #     )
+        #     prompt = expl_prompt + train_prompt + prompt_end
+        #     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
+        #     probs, pred = forward(args, model, tokenizer, input_ids)
+        #     cor = pred == label
+        # else:
+        #     all_third_expls.append(None)
         
         cors.append(cor)
         all_probs.append(probs)
@@ -182,7 +185,7 @@ def eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_ex
     all_probs = np.array(all_probs)
     print("Average accuracy {:.6f} - {}".format(acc, subject))
 
-    return cors, acc, all_probs, all_new_expls
+    return cors, acc, all_probs#, all_third_expls
 
 
 def main(args):
@@ -203,17 +206,18 @@ def main(args):
         ]
     )
     
-    oai_client = Openai(
-        apis=API_INFOS[args.expl_model_name]
-    )
+    # explanations have been generated in expls_OpenAI-GPT-4o-mini_sep_taxo_path_gen_3rd
+    # oai_client = Openai(
+    #     apis=API_INFOS[args.expl_model_name]
+    # )
     # res = oai_client.call("hello")
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
     if not os.path.exists(os.path.join(args.save_dir, "results_{}".format(args.exp_name))):
         os.makedirs(os.path.join(args.save_dir, "results_{}".format(args.exp_name)))
-    if not os.path.exists(os.path.join(args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_2nd".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test")):
-        os.makedirs(os.path.join(args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_2nd".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test"))
+    # if not os.path.exists(os.path.join(args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_3rd_use_choosing_expl".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test")):
+    #     os.makedirs(os.path.join(args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_3rd_use_choosing_expl".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test"))
 
     all_cors = []
     subject_cors = {}
@@ -241,13 +245,14 @@ def main(args):
         test_expls_df = pd.read_csv(
             os.path.join(
                 args.expl_dir,
-                "expls_{}_sep_taxo_path_{}".format(args.expl_model_name, args.taxo_path_src),
+                "expls_{}_sep_taxo_path_{}_3rd".format(args.expl_model_name, args.taxo_path_src),
                 "test",
-                subject + "_expls.csv",
+                subject + "_3rd_expls.csv",
             )
         )
 
-        cors, acc, probs, all_new_expls = eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_expls_df, oai_client)
+        # cors, acc, probs, all_third_expls = eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_expls_df, oai_client)
+        cors, acc, probs = eval(args, subject, model, tokenizer, dev_df, test_df, dev_expls_df, test_expls_df)
         subject_cors[subject] = cors.tolist()
         subcats = subcategories[subject]
         for subcat in subcats:
@@ -267,13 +272,13 @@ def main(args):
             ),
             index=None,
         )
-        test_expls_df["new_explanations"] = all_new_expls
-        test_expls_df.to_csv(
-            os.path.join(
-                args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_2nd".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test", "{}_2nd_expls.csv".format(subject)
-            ),
-            index=None,
-        )
+        # test_expls_df["third_explanations"] = all_third_expls
+        # test_expls_df.to_csv(
+        #     os.path.join(
+        #         args.expl_dir, "expls_{}_sep_taxo_path_{}_{}_wo_choosing_3rd_use_choosing_expl".format(args.expl_model_name, args.taxo_path_src, args.model_name), "test", "{}_3rd_expls.csv".format(subject)
+        #     ),
+        #     index=None,
+        # )
         toc = time.time()
         print("\tTime: {:.3f} s, {} of {}\n".format(toc-tic, subjects.index(subject)+1, len(subjects)))
 
@@ -311,10 +316,10 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir", "-d", type=str, default="/data/qilongma/mmlu_data")
     parser.add_argument("--expl_dir", "-e", type=str, default="explanations")
     parser.add_argument("--save_dir", "-s", type=str, default="results")
-    parser.add_argument("--model_path", "-m", type=str, default="/home/lidong1/qilongma/blob/public_models/Meta-Llama-2-7B-hf")
-    parser.add_argument("--model_name", "-n", type=str, default="Meta-Llama-2-7B")
+    parser.add_argument("--model_path", "-m", type=str, default="/home/lidong1/qilongma/blob/public_models/Meta-Llama-3-8B")
+    parser.add_argument("--model_name", "-n", type=str, default="Meta-Llama-3-8B")
     parser.add_argument("--taxo_path_src", "-tp", type=str, default="gen", choices=["gen", "search"])
     parser.add_argument("--expl_model_name", "-en", type=str, default="OpenAI-GPT-4o-mini")
     args = parser.parse_args()
-    args.exp_name = f"{args.model_name}_feynman_{args.expl_model_name}_sep_question_concept_taxo_path_{args.taxo_path_src}_wo_choosing_2nd"
+    args.exp_name = f"{args.model_name}_feynman_{args.expl_model_name}_sep_question_concept_taxo_path_{args.taxo_path_src}_wo_choosing_3rd_use_choosing_expl"
     main(args)
